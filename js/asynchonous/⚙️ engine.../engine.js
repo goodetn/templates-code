3d44cbbc3e834dc27891b11cef456d59b4d5b078
tottran <tottianddona@gmail.com>
2022-10-16 00:15:52 +0700

    Create engine.js

---
+++ js/asynchonous/⚙️ engine.../engine.js
@@ -0,0 +1,8 @@
+// The JavaScript Engine
+// Một trong những Engine phổ biến nhất của javaScript là Google’s V8 engine. Ví dụ nó được dùng trong trình duyệt Chrome và Nodejs
+
+// Engine này bao gồm 2 components chính:
+
+// Memory Heap -- Các tác vụ phân bổ vùng nhớ sẽ thực hiện ở đây.
+// Call Stack -- Nơi mà các Stack frames trong code của bạn sẽ được thực thi.
+
