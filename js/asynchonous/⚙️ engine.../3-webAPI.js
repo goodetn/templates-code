5079e6e3b3cd738f8edd05942ffd67a1c3fa7c3c
tottran <tottianddona@gmail.com>
2022-10-16 00:31:52 +0700

    Create 3-webAPI.js

---
+++ js/asynchonous/⚙️ engine.../3-webAPI.js
@@ -0,0 +1,4 @@
+// Vậy ở đây chúng ta có Engine, nhưng không chỉ có thế. Chúng ta còn có thêm một thứ gọi là [Web APIs]. 
+// Những [APIs này được cung cấp bởi chính browser mà chúng ta đang sử dụng].Các APIs đó bao gồm DOM, AJAX setTimeout,...
+
+// Và tiếp đó còn có các event loop (sự kiện vòng lặp) thông dụng và callback queue.
