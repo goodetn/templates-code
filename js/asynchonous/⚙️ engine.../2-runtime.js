857e90cc362374c47161942999872ce32a0d7a88
tottran <tottianddona@gmail.com>
2022-10-16 00:31:44 +0700

    Update 2-runtime.js

--- js/asynchonous/⚙️ engine.../2-runtime.js
+++ js/asynchonous/⚙️ engine.../2-runtime.js
@@ -3,10 +3,3 @@
 // Tuy nhiên những APIs này lại [không được cung cấp bởi Engine].
 // Vậy những APIs này từ đâu mà có?
 // Hoá ra thực tế lại có chút phức tạp hơn chúng ta nghỉ.
-
-// =>
-
-// Vậy ở đây chúng ta có Engine, nhưng không chỉ có thế. Chúng ta còn có thêm một thứ gọi là [Web APIs]. 
-// Những [APIs này được cung cấp bởi chính browser mà chúng ta đang sử dụng].Các APIs đó bao gồm DOM, AJAX setTimeout,...
-
-// Và tiếp đó còn có các event loop (sự kiện vòng lặp) thông dụng và callback queue.
