f6e608cade6480b13f2c601aa6bc03a83dcf49dc
tottran <tottianddona@gmail.com>
2022-10-16 00:53:37 +0700

    Update 7-blowing-the-stack.js

--- js/asynchonous/⚙️ engine.../7-blowing-the-stack.js
+++ js/asynchonous/⚙️ engine.../7-blowing-the-stack.js
@@ -10,3 +10,6 @@ foo();
 // Tuy nhiên function foo là một hàm đệ quy, nó tự gọi chính nó mà không hề có bất cứ điều kiện dừng nào.
 
 // Chính vì vậy cứ mỗi lần function foo được gọi, thì chính nó sẽ được add liên tục vào Call Stack mà không thể dừng lại.Hình dưới đây sẽ minh hoạ rõ hơn:
+
+// Tuy nhiên một khi số lượng các hàm "foo" được gọi vượt quá size thực tế của Call Stack 
+// thì lúc đó browser sẽ thực thi một hành động trả ra lỗi giống như hình dưới đây:
