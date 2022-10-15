7092c16ef034e3a3682ba20a28012407eb7393ad
tottran <tottianddona@gmail.com>
2022-10-16 01:09:06 +0700

    Create 6.2-deadlock.js

---
+++ js/asynchonous/⚙️ engine.../6.2-deadlock.js
@@ -0,0 +1,3 @@
+// https://vi.wikipedia.org/wiki/Deadlock
+
+// Deadlock (Khóa chết) là trạng thái xảy ra trong môi trường đa nhiệm (muti-threading) khi hai hoặc nhiều tiến trình đi vào vòng lặp chờ tài nguyên mãi mãi.
